e4b87199038442acaa284f3cc1cbdf1d6f7b4ba9
EELMMAD <elmiramadadi@gmail.com>
2022-11-06 12:48:15 +0100

    CountingSort

---
+++ src/com/elmiramadadi/sortingAlgorithms/CountingSort/Main.java
@@ -0,0 +1,12 @@
+package com.elmiramadadi.sortingAlgorithms.CountingSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {5, 1, 7, 2, 6, 4};
+        var sorter = new CountingSort();
+        sorter.sort(numbers, 7);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
